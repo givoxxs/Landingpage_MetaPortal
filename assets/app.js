f733a1cac2a5fdbbc57dcdb274b840fd26a23e63
givoxxs <toanyogame@gmail.com>
2024-02-14 23:48:40 +0700

    fix animation home slider

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
     $('.slider_top').slick({
-        centerMode: true,
-        centerPadding: '80px',
+        infinity: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay:true,
-        autoplaySpeed: 2000,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        draggable: false,
+        prevArrow:"<button type='button' class='slick-prev slick-arrow pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
         responsive: [
             {
             breakpoint: 768, // 768
@@ -26,5 +32,7 @@ $(document).ready(function(){
             }
             }
         ]
+
+    
     });
   });
